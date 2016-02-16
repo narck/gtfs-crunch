3bcc0492679251fe0ee325cb88565bdfe5a7f1b9
Nicolas Arkkila <nicolas@arkkila.fi>
2016-02-17 00:06:34 +0200

    First implementation of adding Shape GTFS feed items with ActiveRecord

--- app/controllers/gtfs_controller.rb
+++ app/controllers/gtfs_controller.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class GtfsController < ApplicationController
 
   GTFS_FEED_URL = ''
@@ -10,6 +12,26 @@ class GtfsController < ApplicationController
     }
     input_stream = StringIO.new(res.body)
     sha_digest = Digest::SHA1.hexdigest input_stream.string
+    gtfs_zip = Zip::InputStream.open(input_stream)
+
+    while entry = gtfs_zip.get_next_entry
+      if entry.to_s == 'shapes.txt'
+        fields = nil
+
+        CSV::parse(entry.get_input_stream.read) do |row|
+          if fields.nil?
+            fields = row
+          else
+            params = {}
+            fields.each_with_index do |field, i|
+              params[field] = row[i]
+            end
+            kwargs = params.symbolize_keys
+            Shape.create(**kwargs)
+          end
+        end
+      end
+    end
 
     render :json => [sha_digest, res.msg]
   end
