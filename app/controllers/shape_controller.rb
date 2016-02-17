7dfba1aec113dbdf4832c68ec6c749237e406402
Nicolas Arkkila <nicolas@arkkila.fi>
2016-02-17 09:15:04 +0200

    Added new route for processed shapes, changed route name types to symbols

--- app/controllers/shape_controller.rb
+++ app/controllers/shape_controller.rb
@@ -5,4 +5,18 @@ class ShapeController < ApplicationController
     render :json => shape
   end
 
+  def parsed
+    shape = Shape.where(shape_id: params[:id])
+
+    coordinates = []
+
+    shape.each do |row|
+      c = row.shape_pt_lat, row.shape_pt_lon
+      coordinates.push(c)
+    end
+
+    data = {:positions => coordinates}
+    render :json => data
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
-  get 'shape/:id', to: 'shape#get', as: 'shape'
-  get 'gtfs/update', to: 'gtfs#update', as: 'update'
+  get 'shape/:id', to: 'shape#get', as: :shape
+  get 'shape/:id/parsed', to: 'shape#parsed', as: :parsed_shape
+  get 'gtfs/update', to: 'gtfs#update', as: :update
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
