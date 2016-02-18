35b868b06b43e3e5d596a46aa3db83c345fbaea1
Nicolas Arkkila <nicolas@arkkila.fi>
2016-02-18 13:48:04 +0200

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160212103053) do
+ActiveRecord::Schema.define(version: 20160218081648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,12 @@ ActiveRecord::Schema.define(version: 20160212103053) do
     t.date     "feed_start_date"
     t.date     "feed_end_date"
     t.string   "feed_version"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "gtfs_settings", force: true do |t|
+    t.string   "latest_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
