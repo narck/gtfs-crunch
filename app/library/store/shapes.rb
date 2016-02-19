86b8095878547e63446bde5e51c68ab6f1af4424
Nicolas Arkkila <nicolas@arkkila.fi>
2016-02-19 14:23:35 +0200

    Added custom library for app and Store component for Shapes

---
+++ app/library/store/shapes.rb
@@ -0,0 +1,12 @@
+class ShapesStore
+  class << self
+    def get_points_for_named_route(route_name)
+      Shape.joins("
+      JOIN trips ON trips.shape_id=shapes.shape_id
+      JOIN routes ON trips.route_id=routes.route_id")
+          .where(routes: {route_short_name: route_name}, trips: {direction_id: 1}).uniq # TODO: dynamic direction
+    end
+  end
+
+end
+
