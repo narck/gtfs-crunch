8e3240b025a044c1c4c728be8b75150e67133d73
Nicolas Arkkila <nicolas@arkkila.fi>
2016-03-04 09:44:33 +0200

    Small refactor

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -3,7 +3,7 @@ require_relative '../../app/library/store/shapes'
 class LineController < ApplicationController
   def shape
     route_short_name = params[:route_short_name]
-    points = ShapesStore.get_points_for_named_route(route_short_name)
+    points = ShapesStore::get_points_for_named_route(route_short_name)
     coordinates = []
     points.each do |row|
       c = row['shape_pt_lat'], row['shape_pt_lon']
